363bc8f613ed61ad1cedf903fe9e5c4a657d6189
ninadthepade <ninad.thepade@gmail.com>
2020-05-10 02:22:41 +0530

    initial commit with github demo page link

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # advertizer-bidding-app
 Test React App
+
+
+Demo here:  https://ninadthepade.github.io/advertizer-bidding-app/
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Enter the main heading, usually the same as the title.</h1>
+      <p>Be bold in stating your key points. Put them in a list: </p>
+      <ul>
+        <li>The first item in your list</li>
+        <li>The second item</li>
+      </ul>
+      <p>Improve your image by including an image. </p>
+      <p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQ7Yo5hhd6g8D-okkK6YdfgGFhfzYvlKxiWqsPs2LDz7-NG5FR&usqp=CAU" alt="A Great HTML Resource" /></p>
+      <p>Add a link to your favorite <a href="https://www.google.com/">Web site</a>.
+      Break up your page with a horizontal rule or two. </p>
+      <hr />
+      <p>Finally, link to <a href="https://www.google.com/">another page</a> in your own Web site.</p>
+    </div>
+  );
+}
+
+export default App;
