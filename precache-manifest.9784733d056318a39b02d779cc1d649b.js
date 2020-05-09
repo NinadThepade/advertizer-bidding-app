981536bd0bf4e2a318068a83a31d26c0acc555b4
ninadthepade <ninad.thepade@gmail.com>
2020-05-10 02:21:52 +0530

    Updates

---
+++ precache-manifest.9784733d056318a39b02d779cc1d649b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7876370eb1753bc82eb4372b6e1ffdc0",
+    "url": "/advertizer-bidding-app/index.html"
+  },
+  {
+    "revision": "c9d81a79881509c443db",
+    "url": "/advertizer-bidding-app/static/css/main.49104861.chunk.css"
+  },
+  {
+    "revision": "0d418f6458c7b7a978a1",
+    "url": "/advertizer-bidding-app/static/js/2.21161929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/advertizer-bidding-app/static/js/2.21161929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d81a79881509c443db",
+    "url": "/advertizer-bidding-app/static/js/main.4ee196b2.chunk.js"
+  },
+  {
+    "revision": "929b11dcd79c265a34ca",
+    "url": "/advertizer-bidding-app/static/js/runtime-main.943c950b.js"
+  }
+]);
